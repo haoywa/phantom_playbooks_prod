572a652727e50fa7c99d87368e6cc6949c63c2af
Haoyu Wang <haoyuw@splunk.com>
2023-10-04 05:59:56 +0000

    test

---
+++ custom_functions/get_current_environment_haoyutest2_copy.py
@@ -0,0 +1,33 @@
+def get_current_environment_haoyutest2_copy(**kwargs):
+    """
+    Returns the current running environment (phantom-dev.transurban.com or phantom.transurban.com) to allow swapping test/prod variables automatically in playbooks.
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        environment
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+    
+    # Write your custom code here...
+    from urllib.parse import urlparse
+    
+    # Return only the netloc section of the URL
+    try:
+        outputs = {
+            'environment': urlparse(phantom.get_base_url())[1],
+            'status': 'success',
+            'message': 'Successfully returned current SOAR environment',
+        }
+        phantom.debug('Phantom URL: {}'.format(outputs['environment']))
+    except Exception as e:
+        outputs = {
+            'status': 'failed',
+            'message': 'Exception occured: {}'.format(e),
+        }
+    
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
